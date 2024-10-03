11f3bcba05bb93fb5142e409f11a37c52901dd2b
Ryan Parrish <rparrish@nxt.tech>
2024-10-03 14:32:27 -0400

    wrap it

--- handler.py
+++ handler.py
@@ -11,7 +11,23 @@ def hello(event, context):
     try:
         import lxml.etree as etree
     except Exception as e:
-        return {"statusCode": 200, "body": e}
+        ex_type, ex_value, ex_traceback = sys.exc_info()
+
+        # Extract unformatter stack traces as tuples
+        trace_back = traceback.extract_tb(ex_traceback)
+
+        # Format stacktrace
+        stack_trace = list()
+
+        for trace in trace_back:
+            stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" % (trace[0], trace[1], trace[2], trace[3]))
+
+        print("Exception type : %s " % ex_type.__name__)
+        print("Exception message : %s" %ex_value)
+        print("Stack trace : %s" %stack_trace)
+        
+        
+        return {"statusCode": 200, "body": json.dumps({"type": ex_type.__name__, "message": ex_value, "trace": stack_trace})}
     
 
     return {"statusCode": 200, "body": json.dumps(body)}
